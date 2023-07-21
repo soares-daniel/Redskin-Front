22fb51017943015b04fe6176a0ea1c99b89a9f65
silvestri-gilles <gilles.silvestri@gmail.com>
2023-07-21 20:10:59 +0200

    fixed event deletion

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { addEvent, deleteEvent, fetchEvents } from '@/controllers/calendarController';
+import { addEvent, fetchEvents } from '@/controllers/calendarController';
 import useEventsData from '@/hooks/useEventsData';
 import { transformEvents } from '@/utils/eventTransform';
 import { DateSelectArg, EventClickArg } from 'fullcalendar';
@@ -11,12 +11,13 @@ import { createEventId } from './event-utils';
 import interactionPlugin from '@fullcalendar/interaction';
 import useSuperUserToken from '@/hooks/auth';
 import { useCookies } from 'react-cookie';
+import useDeleteEvent from '@/hooks/useDeleteEvent';
 
 
 export default function Calendar() {
   const { data, loading, error } = useEventsData();
   //const { addEvent } = useAddEvent();
-  //const { deleteEvent } = useDeleteEvent();
+  const { deleteEvent } = useDeleteEvent();
   const token = useSuperUserToken();
   const [cookies] = useCookies(['superUserToken']);
   
@@ -37,7 +38,7 @@ export default function Calendar() {
 
   const handleDeleteEvent = async (clickInfo: EventClickArg) => {
     if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      const eventId = Number(clickInfo.event.id);
+      const eventId = (clickInfo.event.id);
       await deleteEvent(eventId);
       // After deleting the event from the server, remove it from the calendar
       clickInfo.event.remove();

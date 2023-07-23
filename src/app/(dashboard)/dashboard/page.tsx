7014218f1126367b566f69556d76bfb7d93ac98b
silvestri-gilles <gilles.silvestri@gmail.com>
2023-07-23 16:15:00 +0200

    fixed eventcards

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -3,7 +3,6 @@ import React, { useEffect } from "react";
 import Layout from "@/components/layout";
 import Calendar from "@/components/calendar";
 import EventCard from "@/components/eventcard";
-import Test from "@/components/Test";
 import useEventsData from "@/hooks/useEventsData";
 import { useRouter } from "next/navigation";
 
@@ -11,7 +10,6 @@ import { useRouter } from "next/navigation";
 export default function Dashboard() {
   const router = useRouter();
   const { data: events, loading, error } = useEventsData();
-  
 
   useEffect(() => {
     if (error && error.message === 'Not Authorized') {
@@ -27,9 +25,7 @@ export default function Dashboard() {
     <Layout>
       <div className="flex h-full gap-4 pt-4">
         <div className="w-1/4 overflow-y-auto h-screen p-4">
-          {events.sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime()).map((event, index) => (
-            <EventCard event={event} key={index}/>
-          ))}
+          <EventCard events={events} />
         </div>
         <div className="w-3/4 border border-gray-400 mx-auto p-2">
           <Calendar />
--- src/components/eventcard.tsx
+++ src/components/eventcard.tsx
@@ -1,36 +1,41 @@
 "use client"
-import { FC, useEffect, useState } from 'react';
-import { fetchEvents } from '@/controllers/calendarController';
+import React, { FC, useEffect, useState } from 'react';
 import { EventDataTransformed } from '@/utils/eventTransform';
 import useEventsData from '@/hooks/useEventsData';
 import { Event } from '@/types/types';
 import { FullCalendarEvent } from '@/utils/eventTransform';
 
+export default function EventCard({ events }: { events: FullCalendarEvent[] }) {
+  // Sort the events by their start dates
+  const sortedEvents = [...events].sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime());
 
-export default function EventCard({ event }: { event: FullCalendarEvent }) {
   return (
-    <div className="bg-white rounded shadow mb-4 p-6">
-      <h3 className="text-xl font-bold">{event.title}</h3>
-      <p className="text-sm text-gray-600">
-        {new Date(event.start).toLocaleDateString()} -{' '}
-        {new Date(event.end).toLocaleDateString()}
-      </p>
-      {event.extendedProps && (
-        <>
+    <>
+      {sortedEvents.map((event, index) => (
+        <div className="bg-white rounded shadow mb-4 p-6" key={index}>
+          <h3 className="text-xl font-bold">{event.title}</h3>
           <p className="text-sm text-gray-600">
-            Time: {event.start.toString().slice(16, 21)} -{' '} {event.end.toString().slice(16, 21)}
+            {new Date(event.start).toLocaleDateString()} -{' '}
+            {new Date(event.end).toLocaleDateString()}
           </p>
-          <p className="text-sm text-gray-600">
-            Description: {event.extendedProps.description}
-          </p>
-          <p className="text-sm text-gray-600">
-            Created by: {event.extendedProps.createdBy}
-          </p>
-          <p className="text-sm text-gray-600">
-            Event Type: {event.extendedProps.eventType}
-          </p>
-        </>
-      )}
-    </div>
+          {event.extendedProps && (
+            <>
+              <p className="text-sm text-gray-600">
+                Time: {event.start.toString().slice(16, 21)} -{' '} {event.end.toString().slice(16, 21)}
+              </p>
+              <p className="text-sm text-gray-600">
+                Description: {event.extendedProps.description}
+              </p>
+              <p className="text-sm text-gray-600">
+                Created by: {event.extendedProps.createdBy}
+              </p>
+              <p className="text-sm text-gray-600">
+                Event Type: {event.extendedProps.eventType}
+              </p>
+            </>
+          )}
+        </div>
+      ))}
+    </>
   );
 }

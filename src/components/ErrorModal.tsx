f8be65cb9b21cf71833588112953f4409e87eebb
silvestri-gilles <gilles.silvestri@gmail.com>
2023-08-14 17:07:08 +0200

    added page refresh button

--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -7,6 +7,11 @@ type ErrorModalProps = {
   };
 
 export default function ErrorModal({ isOpen, onRequestClose, errorMessage }: ErrorModalProps) {
+
+  const handleRefresh = () => {
+    window.location.reload();
+};
+
     return (
     <Modal
     isOpen={isOpen} 
@@ -28,14 +33,18 @@ export default function ErrorModal({ isOpen, onRequestClose, errorMessage }: Err
           height: 'fit-content', 
         }
       }}>
-        <h2 className="modal-header">An Error occured</h2>
-      <div className={`errmodal ${isOpen ? 'is-open' : ''}`}>
-        <div className="errmodal-content">
-          <p>{errorMessage}</p>
-          <button onClick={onRequestClose} className="modal-close-button bg-red-500 bg-opacity-60 text-white p-1 rounded hover:bg-red-600 absolute top-4 right-4">Close</button>
-        </div>
-      </div>
-      </Modal>
+        <h2 className="modal-header">Oops something went wrong!</h2>
+            <div className={`errmodal ${isOpen ? 'is-open' : ''}`}>
+                <div className="errmodal-content">
+                    <p>{errorMessage}</p>
+                    <hr className="my-4"/> 
+                    <div className="text-center"> 
+                        <button onClick={handleRefresh} className="modal-refresh-button bg-blue-500 text-white p-1 rounded hover:bg-blue-600">Refresh Page</button>
+                    </div>
+                    <button onClick={onRequestClose} className="modal-close-button bg-red-500 bg-opacity-60 text-white p-1 rounded hover:bg-red-600 absolute top-4 right-4">Close</button>
+                </div>
+            </div>
+        </Modal>
     );
   }
   
